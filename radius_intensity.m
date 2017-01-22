1ef9ea754d4ba58454da00d69eb2689521ba8d5f
meowoodie <woodielove@163.com>
2017-01-22 11:28:48 -0500

    add preprocessing for the tifs images

--- radius_intensity.m
+++ radius_intensity.m
@@ -8,26 +8,48 @@ addpath(genpath('reader/'));
 addpath(genpath('preproc/'));
 addpath(genpath('utils/'));
 
-%% Read data and other preliminary work
+%% Read data from ITO
 
-top_dir = '/Users/woodie/Desktop/ISyE/data/ITO_D10_patterns1';
-minuteRange = 0:1;
-secondRange = 0:59;
+% top_dir = '/Users/woodie/Desktop/ISyE/data/ITO_D10_patterns1';
+% minuteRange = 0:1;
+% secondRange = 0:59;
+% 
+% % Read data from DM4 file folder
+% [stack3D,xv,yv] = K2importDM4(top_dir, minuteRange, secondRange);
+% mats = stack3D(:,:,1:1500);
+% 
+% % Check the result dir, if it didn't exist, then create it.
+% res_dir = '/Users/woodie/Desktop/IT0_D10_rearranged_res';
+% if ~exist(res_dir, 'dir')
+%     mkdir(res_dir);
+% end
 
-% Read data from DM4 file folder
-[stack3D,xv,yv] = K2importDM4(top_dir, minuteRange, secondRange);
-mats = stack3D(:,:,1:1500);
+% % Init parameter
+% shape  = [1024, 1024];
+% center = [525 520];
+% radius_range = [0, 500];
+% ring_width   = 2;
+% angle_step   = 360;
 
-% Check the result dir, if it didn't exist, then create it.
-res_dir = '/Users/woodie/Desktop/IT0_D10_rearranged_res';
-if ~exist(res_dir, 'dir')
-    mkdir(res_dir);
+%% Read data from tifs images
+
+% Define the working directory where the images store.
+workingDir = '/Users/woodie/Desktop/ISyE/material_oldversion/tifs';
+% Locate the directory of the image files.
+imageNames = dir(fullfile(workingDir, '*.tif'));
+imageNames = {imageNames.name}';
+imageNames = sort_nat(imageNames);
+
+mats = [];
+for i=1:length(imageNames)
+    img = imread(fullfile(workingDir, imageNames{i}));
+    mats = cat(3, mats, img);
 end
 
 % Init parameter
-shape  = [1024, 1024];
-center = [525 520];
-radius_range = [0, 500];
+shape  = [512, 512];
+center = [275, 249];
+radius_range = [0, 200];
 ring_width   = 2;
 angle_step   = 360;
 
@@ -40,18 +62,18 @@ angle_step   = 360;
 
 %% Get the radius-intensity matrix for each of the frame
 
-mats_size  = 500; % size(mats, 3);
+mats_size  = size(mats, 3);
 masks_size = size(ring_masks, 3);
 radius_intensity_mat = ones(mats_size, masks_size);
 for i=1:mats_size
     for j=1:masks_size
         fprintf('Processing %d mat and %d ring\n', i, j);
-        ring = mats(:,:,i) .* ring_masks(:,:,j);
+        ring = int16(mats(:,:,i)) .* int16(ring_masks(:,:,j));
         radius_intensity_mat(i,j) = mean(ring(:));
     end
 end
 mean_r_i = mean(radius_intensity_mat);
-x_axis   = 0:2:500;
+x_axis   = 0:2:198;
 
 figure; 
 plot(x_axis, mean_r_i); 
